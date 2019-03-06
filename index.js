c220ff41a8d6356acf3bffe696e8c8f0ccce15fc
jjblues86 <jeromejoof@yahoo.com>
2019-03-06 00:12:03 -0800

    initial commit

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ const mongooseOptions = {
 };
 mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 
-require('./src/app.js').start(process.env.PORT);
+require('./api-server/src/app.js').start(process.env.PORT);
